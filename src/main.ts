1b8f3c37b5682976f7c35a0d2c5a90c9b5c9144a
Samuel Martin <samumartinfrias@gmail.com>
2024-01-30 15:53:32 +0000

    Initial commit without the pieces

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { invoke } from "@tauri-apps/api/tauri";
+declare var $: any; // for jQuery
+declare var Chessboard: any; // for chessboard.js
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  // console.log(Chessboard);
+  // console.log($);
+  var config = {
+    draggable: true,
+    moveSpeed: 'slow',
+    snapbackSpeed: 500,
+    snapSpeed: 100,
+    position: 'start'
+  }
+  var board = Chessboard("board", config);
+  $("#startBtn").on("click", board.start);
+  $("#clearBtn").on("click", board.clear);
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
